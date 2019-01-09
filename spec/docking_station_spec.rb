de3c73fe77b8a7176a4e09870c24000a6fedebd6
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-09 16:22:54 +0000

    error message when there's a bike in docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,9 +9,9 @@ describe DockingStation do
   describe "#release_bike" do
 
     it "should release a bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
     end
 
     it "should give error when no bikes are docked and release_bike is used" do
@@ -19,10 +19,19 @@ describe DockingStation do
     end
   end
 
-  it "docks a bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe "#dock" do
+
+    it "should dock a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+
+    it "should give error when more than one bike is tried to be docked" do
+      bike, bike2 = Bike.new
+      subject.dock(bike)
+      expect {subject.dock(bike2)}.to raise_error "Docking station full"
+    end
   end
 
 
