a3ffb2fe990c42cfa343bbdde79549a5960b0f5f
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-09 13:46:25 +0000

    Changes to align with project walkthroughs

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,22 +1,7 @@
+require_relative 'bike'
+
 class DockingStation
-  attr_reader :docked_bikes
-
-  def initialize
-    @docked_bikes = []
-  end
-
   def release_bike
     Bike.new
-  end
-
-  def dock(bike)
-    @docked_bikes << bike
-  end
-
-end
-
-class Bike
-  def working?
-    true
   end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,5 @@
-require 'docking_station'
+require 'bike'
+
 describe Bike do
   it {is_expected.to respond_to :working?}
 end
