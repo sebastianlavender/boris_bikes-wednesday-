7436a9602487947b02a35bd5b0e8bd1801aac339
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-10 15:53:58 +0000

    question 19, isolating tests with Doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,11 +5,12 @@ describe DockingStation do
   it {is_expected.to respond_to :release_bike}
   it {is_expected.to respond_to(:dock).with(1).argument}
   it {is_expected.to respond_to :capacity}
-  let(:bike) {Bike.new}
+  # let(:bike) {Bike.new}
 
   describe "#release_bike" do
 
     it "should release a bike" do
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -19,6 +20,7 @@ describe DockingStation do
     end
 
     it 'should not release a bike if it has been reported as broken' do
+      bike = double(:bike)
       bike.report_broken
       subject.dock(bike)
       expect {subject.release_bike}.to raise_error 'No working bikes available'
@@ -28,12 +30,14 @@ describe DockingStation do
   describe "#dock" do
 
     it "should dock a bike" do
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "should give error when more than one bike is tried to be docked" do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new) }
+      bike = double(:bike)
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(bike) }
       expect {subject.dock(bike)}.to raise_error "Docking station full"
     end
   end
